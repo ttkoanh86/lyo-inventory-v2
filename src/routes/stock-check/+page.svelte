84396387ec2593e528216c6fff450e92c1735024
ttkoanh86 <ttkoanh86@gmail.com>
2026-09-14 17:59:24 +0700

    Create +page.svelte

---
+++ src/routes/stock-check/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { onMount } from "svelte";
+    // Kế thừa lại ItemViewV2 từ thư mục dashboard
+    import ItemViewV2 from "../dashboard/ItemViewV2.svelte";
+
+    let k = $state(0);
+
+    onMount(() => {
+        const token = sessionStorage.getItem("token") || localStorage.getItem("token");
+        if (!token) {
+            goto("/authentication");
+        }
+    });
+</script>
+
+{#key k}
+    <ItemViewV2 />
+{/key}
