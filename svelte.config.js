47472359f8192f43a9c051813e6a5d9b82fb4a1f
ttkoanh86 <ttkoanh86@gmail.com>
2026-09-25 18:29:27 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,24 +3,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	ssr: false,
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: "index.html"
-		}),
-
-		prerender: {
-			entries: []
-		}
+			fallback: '200.html', // 🟢 BẮT BUỘC ĐỔI THÀNH '200.html' ĐỂ SVELTEKIT TẠO FILE FALLBACK SPA
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
