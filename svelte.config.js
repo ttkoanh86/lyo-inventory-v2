4c0244a7ec0b604f6bb0b57b616872825e701f28
ttkoanh86 <ttkoanh86@gmail.com>
2026-09-25 18:37:15 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,20 +4,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	ssr: false,
-
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '200.html', // Tạo file fallback 200.html cho SPA
+			fallback: 'index.html', // 🔥 Tự động tạo file index.html chuẩn cho ứng dụng SPA
 			precompress: false,
-			strict: true
-		}),
-
-		prerender: {
-			entries: ['*'] // 🟢 ĐỔI THÀNH ['*'] ĐỂ SVELTEKIT TỰ TẠO TRANG INDEX.HTML VÀ CÁC TRANG CÒN LẠI
-		}
+			strict: false // Bỏ qua strict để không bị chặn khi build
+		})
 	}
 };
 
